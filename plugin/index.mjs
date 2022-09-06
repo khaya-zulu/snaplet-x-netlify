b236d63dfeb6a4114b30985b226dfaf5b4bf54f6
khaya-zulu <kzulu321@gmail.com>
2022-09-06 19:13:12 +0200

    add plugin

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -1,9 +1,7 @@
 export const onPreBuild = async function ({
   utils: { build, status, cache, run, git },
 }) {
-  await run.command(
-    'curl -sS "https://app.snaplet.dev/get-cli/" | bash &> "/dev/null"'
-  );
+  await run.command('curl -sS "https://app.snaplet.dev/get-cli/"');
 
   await run.command("snaplet database create --git --latest");
   await run.command("snaplet database url --git");
