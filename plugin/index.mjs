ff215b96deb504d3a23bb493ebf1700c90f63bdb
khaya-zulu <kzulu321@gmail.com>
2022-09-06 19:16:03 +0200

    add plugin

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -1,5 +1,11 @@
 export const onPreBuild = async function ({
-  utils: { build, status, cache, run, git },
+  utils: {
+    build,
+    status,
+    cache,
+    run,
+    utils: { run },
+  },
 }) {
   await run.command('curl -sS "https://app.snaplet.dev/get-cli/"');
 
