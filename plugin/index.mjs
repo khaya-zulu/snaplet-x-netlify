389e242dc9596a116986d771f57f2e05bff10dc2
khaya-zulu <kzulu321@gmail.com>
2022-09-08 14:53:24 +0200

    add git branch command

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -2,9 +2,11 @@ export const onPreBuild = function ({ netlifyConfig }) {
   netlifyConfig.build.environment.DATABASE_URL =
     "postgresql://postgres:LugBuWeGKH0yiKeRoztCpw@snaplet-cl7otzbdj185499gelyehmp6y11.fly.dev:5432/preview-deploy";
 
-  const newCommand = `git branch --show-current`;
+  const newCommand = `node -p "process.env"`;
 
   netlifyConfig.build.command = netlifyConfig.build.command
     ? `${netlifyConfig.build.command} && ${newCommand}`
     : newCommand;
+
+  console.log({ env: processe.env });
 };
