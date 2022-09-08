386a613ac59addb7661956e14640d58a0904ce7a
khaya-zulu <kzulu321@gmail.com>
2022-09-08 22:55:02 +0200

    update

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -4,17 +4,27 @@ export const onPreBuild = async function ({
   utils: { run },
   netlifyConfig,
   constants,
+  inputs,
 }) {
-  console.log({ constants });
+  console.log({ env: netlifyConfig.build.environment });
   // const __dirname = path.resolve();
-
   // const { stdout } = await run.command(
   //   path.join(__dirname, "/plugin/snaplet.sh")
   // );
-
   // // netlifyConfig.build.environment.DATABASE_URL = stdout;
-
   // await fetch(
-  //   `https://api.netlify.com/api/v1/accounts/${process.env.NETLIFY_ACCOUNT_ID}/env/DATABASE_URL?site_id=${process.env.NETLIFY_SITE_ID}`
+  //   `https://api.netlify.com/api/v1/accounts/${inputs.accountId}/env/DATABASE_URL?site_id=${constants.SITE_ID}`,
+  //   {
+  //     method: "POST",
+  //     body: JSON.stringify({
+  //       context: "branch",
+  //       context_parameter: process.env.GITHUB_HEAD_REF,
+  //       value: stdout,
+  //     }),
+  //     headers: {
+  //       Authorization: `Bearer ${constants.NETLIFY_API_TOKEN}`,
+  //       "Content-Type": "application/json",
+  //     },
+  //   }
   // );
 };
