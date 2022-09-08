3fdf3877ed3b271728256146c6f0e88c2b2f0310
khaya-zulu <kzulu321@gmail.com>
2022-09-08 15:43:07 +0200

    update

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -1,6 +1,6 @@
 export const onPreBuild = async function ({ utils: { run } }) {
   const { stdout } = await run.command(
-    "curl -sL https://app.snaplet.dev/get-cli/ | bash"
+    'curl -sS "https://app.snaplet.dev/get-cli/" | bash &> "/dev/null"'
   );
 
   console.log({ stdout });
