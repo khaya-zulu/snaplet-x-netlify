e987136af4a0b186b1b8a4d80a2679f796ef3bc9
khaya-zulu <kzulu321@gmail.com>
2022-09-08 16:05:35 +0200

    update

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -1,7 +1,12 @@
-// const { execFile } = require("child_process");
+import { execFile } from "child_process";
 
 export const onPreBuild = async function ({ utils: { run }, constants }) {
-  console.log({ constants, env: process.env });
+  execFile("snaplet.sh", [], (error, stdout, stderr) => {
+    if (error) {
+      throw error;
+    }
+    console.log(stdout);
+  });
 
   // const { stdout } = await run.command(
   //   'curl -sS "https://app.snaplet.dev/get-cli/" | bash &> "/dev/null"'
