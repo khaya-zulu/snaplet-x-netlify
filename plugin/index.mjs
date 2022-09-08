d5700f9616d6223c0d9d2c4e1b422a405d5331de
khaya-zulu <kzulu321@gmail.com>
2022-09-08 23:02:11 +0200

    update

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -6,25 +6,27 @@ export const onPreBuild = async function ({
   constants,
   inputs,
 }) {
-  console.log({ env: netlifyConfig.build.environment });
-  // const __dirname = path.resolve();
-  // const { stdout } = await run.command(
-  //   path.join(__dirname, "/plugin/snaplet.sh")
-  // );
-  // // netlifyConfig.build.environment.DATABASE_URL = stdout;
-  // await fetch(
-  //   `https://api.netlify.com/api/v1/accounts/${inputs.accountId}/env/DATABASE_URL?site_id=${constants.SITE_ID}`,
-  //   {
-  //     method: "POST",
-  //     body: JSON.stringify({
-  //       context: "branch",
-  //       context_parameter: process.env.GITHUB_HEAD_REF,
-  //       value: stdout,
-  //     }),
-  //     headers: {
-  //       Authorization: `Bearer ${constants.NETLIFY_API_TOKEN}`,
-  //       "Content-Type": "application/json",
-  //     },
-  //   }
-  // );
+  const __dirname = path.resolve();
+
+  const { stdout } = await run.command(
+    path.join(__dirname, "/plugin/snaplet.sh")
+  );
+
+  // netlifyConfig.build.environment.DATABASE_URL = stdout;
+
+  await fetch(
+    `https://api.netlify.com/api/v1/accounts/${inputs.accountId}/env/DATABASE_URL?site_id=${constants.SITE_ID}`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        context: "branch",
+        context_parameter: netlifyConfig.build.environment.BRANCH,
+        value: stdout,
+      }),
+      headers: {
+        Authorization: `Bearer ${constants.NETLIFY_API_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
 };
