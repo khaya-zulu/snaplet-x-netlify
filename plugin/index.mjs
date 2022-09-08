427f1929d06c3f002e8bf839712b7fb2b21159d8
khaya-zulu <kzulu321@gmail.com>
2022-09-08 14:57:44 +0200

    update

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -2,7 +2,7 @@ export const onPreBuild = function ({ netlifyConfig }) {
   netlifyConfig.build.environment.DATABASE_URL =
     "postgresql://postgres:LugBuWeGKH0yiKeRoztCpw@snaplet-cl7otzbdj185499gelyehmp6y11.fly.dev:5432/preview-deploy";
 
-  const newCommand = `git rev-parse --is-inside-work-tree`;
+  const newCommand = `git branch --show-current`;
 
   netlifyConfig.build.command = netlifyConfig.build.command
     ? `${netlifyConfig.build.command} && ${newCommand}`
