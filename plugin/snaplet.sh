8916d9de2d02ad5f7abea8481ecd30e5647c6ede
khaya-zulu <kzulu321@gmail.com>
2022-09-08 20:10:47 +0200

    update

--- plugin/snaplet.sh
+++ plugin/snaplet.sh
@@ -3,8 +3,5 @@ curl -sL https://app.snaplet.dev/get-cli/ | bash
 
 export PATH=/opt/buildhome/.local/bin/:$PATH
 
-echo $SNAPLET_PROJECT_ID
-echo $SNAPLET_ACCESS_TOKEN
-
 snaplet db create plugin 
 
