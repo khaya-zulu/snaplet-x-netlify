874d8f84140caa7553f38c66611effbe8a431888
khaya-zulu <kzulu321@gmail.com>
2022-09-08 13:08:54 +0200

    update

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -1,4 +1,6 @@
 export const onPreBuild = async function ({ netlifyConfig }) {
+  console.log({ env: netlifyConfig.build.environment });
+
   netlifyConfig.build.environment.DATABASE_URL =
-    "postgres://snaplet_x_netlify_user:V3OK6GWhQgpzhSOF7tlSboG57H4ko6CF@dpg-ccb002pa6gdmn7sdarl0-a.oregon-postgres.render.com/snaplet_x_netlify";
+    "postgresql://postgres:LugBuWeGKH0yiKeRoztCpw@snaplet-cl7otzbdj185499gelyehmp6y11.fly.dev:5432/preview-deploy";
 };
