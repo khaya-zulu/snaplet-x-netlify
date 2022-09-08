63dc14a30025cca4bec58f37101f8faab912895c
khaya-zulu <kzulu321@gmail.com>
2022-09-08 16:37:22 +0200

    update

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -1,6 +1,14 @@
 import { execFile } from "child_process";
 import path from "path";
 
+import fs from "fs/promises";
+const path = "path/to/folder";
+
+async function readDir(path) {
+  const files = await fs.readdir(path);
+  console.log(files); // array of file names
+}
+
 export const onPreBuild = async function ({ utils: { run }, constants }) {
   // const __dirname = path.resolve();
 
@@ -17,7 +25,11 @@ export const onPreBuild = async function ({ utils: { run }, constants }) {
   //   );
   // });
 
-  console.log(constants);
+  const __dirname = path.resolve();
+
+  const files = await fs.readDir(__dirname);
+
+  console.log(files);
 
   // const { stdout } = await run.command(
   //   "curl -sL https://app.snaplet.dev/get-cli/ | bash"
