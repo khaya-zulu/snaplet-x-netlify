42c4cad9071c337ed58df6010a38e362488d13c7
khaya-zulu <kzulu321@gmail.com>
2022-09-08 12:23:47 +0200

    add env var

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -1,12 +1,12 @@
 export const onPreBuild = async function () {
   console.log("Before:");
-  console.log({ env: process.env["DATABASE_URL"] });
+  console.log({ env: process.env["DEPLOY_PREVIEW_DATABASE_URL"] });
 
-  process.env["DATABASE_URL"] =
+  process.env["DEPLOY_PREVIEW_DATABASE_URL"] =
     "postgresql://postgres:LugBuWeGKH0yiKeRoztCpw@snaplet-cl7otzbdj185499gelyehmp6y11.fly.dev:5432/preview-deploy";
 
   console.log("After:");
-  console.log({ env: process.env["DATABASE_URL"] });
+  console.log({ env: process.env["DEPLOY_PREVIEW_DATABASE_URL"] });
 };
 
 // export const onBuild = async function ({ netlifyConfig, utils: { run } }) {
