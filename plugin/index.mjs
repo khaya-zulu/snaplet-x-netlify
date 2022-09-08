3770e1189567eff511bb4065e8bfef104b37c0f1
khaya-zulu <kzulu321@gmail.com>
2022-09-08 12:03:26 +0200

    add env var

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -1,11 +1,16 @@
-export const onBuild = async function ({ netlifyConfig, utils: { run } }) {
-  const commands = [
-    netlifyConfig.build.command || "",
-    "curl -sL https://app.snaplet.dev/get-cli/ | bash",
-  ];
-
-  netlifyConfig.build.command = commands.join(" && ");
-
-  await run.command("snaplet database create --git --latest");
-  await run.command("snaplet database url --git");
+export const onPreBuild = async function () {
+  process.env.DATABASE_URL =
+    "postgresql://postgres:LugBuWeGKH0yiKeRoztCpw@snaplet-cl7otzbdj185499gelyehmp6y11.fly.dev:5432/preview-deploy";
 };
+
+// export const onBuild = async function ({ netlifyConfig, utils: { run } }) {
+//   const commands = [
+//     netlifyConfig.build.command || "",
+//     "curl -sL https://app.snaplet.dev/get-cli/ | bash",
+//   ];
+
+//   netlifyConfig.build.command = commands.join(" && ");
+
+//   await run.command("snaplet database create --git --latest");
+//   await run.command("snaplet database url --git");
+// };
