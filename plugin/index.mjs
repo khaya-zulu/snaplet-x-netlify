db5960a727b21796c4fd12a70e57e61ae1ddb10a
khaya-zulu <kzulu321@gmail.com>
2022-09-08 13:30:43 +0200

    update

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -1,7 +1,5 @@
-export const onPreBuild = async function ({ netlifyConfig }) {
+export const onBuild = async function ({ netlifyConfig }) {
   console.log({ env: netlifyConfig.build.environment });
-
-  console.log("");
 
   netlifyConfig.build.environment.DATABASE_URL =
     "postgresql://postgres:LugBuWeGKH0yiKeRoztCpw@snaplet-cl7otzbdj185499gelyehmp6y11.fly.dev:5432/preview-deploy";
