d324db11966ab0a6a3dc326f3947ecb8be91a05d
khaya-zulu <kzulu321@gmail.com>
2022-09-09 11:34:12 +0200

    test

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -1,3 +1,4 @@
+import axios from "axios";
 import path from "path";
 
 export const onPreBuild = async function ({ utils: { run } }) {
@@ -8,21 +9,36 @@ export const onPreBuild = async function ({ utils: { run } }) {
       path.join(__dirname, "/plugin/snaplet.sh")
     );
 
-    await fetch(
+    await axios.post(
       `https://api.netlify.com/api/v1/accounts/${inputs.accountId}/env/DATABASE_URL?site_id=${constants.SITE_ID}`,
       {
-        method: "POST",
-        body: JSON.stringify({
-          context: "branch",
-          context_parameter: netlifyConfig.build.environment.BRANCH,
-          value: stdout,
-        }),
+        context: "branch",
+        context_parameter: netlifyConfig.build.environment.BRANCH,
+        value: stdout,
+      },
+      {
         headers: {
           Authorization: `Bearer ${constants.NETLIFY_API_TOKEN}`,
           "Content-Type": "application/json",
         },
       }
     );
+
+    // await fetch(
+    //   `https://api.netlify.com/api/v1/accounts/${inputs.accountId}/env/DATABASE_URL?site_id=${constants.SITE_ID}`,
+    //   {
+    //     method: "POST",
+    //     body: JSON.stringify({
+    //       context: "branch",
+    //       context_parameter: netlifyConfig.build.environment.BRANCH,
+    //       value: stdout,
+    //     }),
+    //     headers: {
+    //       Authorization: `Bearer ${constants.NETLIFY_API_TOKEN}`,
+    //       "Content-Type": "application/json",
+    //     },
+    //   }
+    // );
   }
 
   // await axios.post(
