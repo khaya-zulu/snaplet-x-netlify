127bcc69cbced63822fe8c326cac276b2bfd4294
khaya-zulu <kzulu321@gmail.com>
2022-09-09 13:27:41 +0200

    update

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -1,5 +1,6 @@
-import axios from "axios";
 import path from "path";
+
+import fetch from "node-fetch";
 
 export const onPreBuild = async function ({
   utils: { run },
@@ -8,20 +9,20 @@ export const onPreBuild = async function ({
   netlifyConfig,
 }) {
   if (process.env.CONTEXT === "deploy-preview") {
-    const __dirname = path.resolve();
+    // const __dirname = path.resolve();
 
-    const { stdout } = await run.command(
-      path.join(__dirname, "/plugin/snaplet.sh")
-    );
+    // const { stdout } = await run.command(
+    //   path.join(__dirname, "/plugin/snaplet.sh")
+    // );
 
-    await axios.default.post(
+    await fetch(
       `https://api.netlify.com/api/v1/accounts/${inputs.accountId}/env/DATABASE_URL?site_id=${constants.SITE_ID}`,
       {
-        context: "branch",
-        context_parameter: netlifyConfig.build.environment.BRANCH,
-        value: stdout,
-      },
-      {
+        body: JSON.stringify({
+          context: "branch",
+          context_parameter: netlifyConfig.build.environment.BRANCH,
+          value: "test",
+        }),
         headers: {
           Authorization: `Bearer ${constants.NETLIFY_API_TOKEN}`,
           "Content-Type": "application/json",
@@ -47,13 +48,13 @@ export const onPreBuild = async function ({
   }
 };
 
-export const onError = async ({ utils: { run } }) => {
-  if (process.env.CONTEXT === "deploy-preview") {
-    const __dirname = path.resolve();
-    try {
-      await run.command(path.join(__dirname, "/plugin/delete.sh"));
-    } catch (err) {
-      console.log("Delete db failed");
-    }
-  }
-};
+// export const onError = async ({ utils: { run } }) => {
+//   if (process.env.CONTEXT === "deploy-preview") {
+//     const __dirname = path.resolve();
+//     try {
+//       await run.command(path.join(__dirname, "/plugin/delete.sh"));
+//     } catch (err) {
+//       console.log("DB does not exist");
+//     }
+//   }
+// };
