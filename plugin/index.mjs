cf38d5629f6a51a64b238840cf9e50eed761c88d
khaya-zulu <kzulu321@gmail.com>
2022-09-09 13:49:34 +0200

    update

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -9,22 +9,20 @@ export const onPreBuild = async function ({
   netlifyConfig,
 }) {
   if (process.env.CONTEXT === "deploy-preview") {
-    // const __dirname = path.resolve();
+    const __dirname = path.resolve();
 
-    // const { stdout } = await run.command(
-    //   path.join(__dirname, "/plugin/snaplet.sh")
-    // );
+    const { stdout } = await run.command(
+      path.join(__dirname, "/plugin/snaplet.sh")
+    );
 
-    console.log({ token: process.env.API_ACCESS_TOKEN });
-
-    const resp = await fetch(
+    await fetch(
       `https://api.netlify.com/api/v1/accounts/${inputs.accountId}/env/DATABASE_URL?site_id=${constants.SITE_ID}`,
       {
         method: "PATCH",
         body: JSON.stringify({
           context: "branch",
           context_parameter: netlifyConfig.build.environment.BRANCH,
-          value: "test",
+          value: stdout,
         }),
         headers: {
           Authorization: `Bearer ${process.env.API_ACCESS_TOKEN}`,
@@ -32,34 +30,16 @@ export const onPreBuild = async function ({
         },
       }
     );
-
-    console.log({ resp });
-
-    // await fetch(
-    //   `https://api.netlify.com/api/v1/accounts/${inputs.accountId}/env/DATABASE_URL?site_id=${constants.SITE_ID}`,
-    //   {
-    //     method: "POST",
-    //     body: JSON.stringify({
-    //       context: "branch",
-    //       context_parameter: netlifyConfig.build.environment.BRANCH,
-    //       value: stdout,
-    //     }),
-    //     headers: {
-    //       Authorization: `Bearer ${constants.NETLIFY_API_TOKEN}`,
-    //       "Content-Type": "application/json",
-    //     },
-    //   }
-    // );
   }
 };
 
-// export const onError = async ({ utils: { run } }) => {
-//   if (process.env.CONTEXT === "deploy-preview") {
-//     const __dirname = path.resolve();
-//     try {
-//       await run.command(path.join(__dirname, "/plugin/delete.sh"));
-//     } catch (err) {
-//       console.log("DB does not exist");
-//     }
-//   }
-// };
+export const onError = async ({ utils: { run } }) => {
+  if (process.env.CONTEXT === "deploy-preview") {
+    const __dirname = path.resolve();
+    try {
+      await run.command(path.join(__dirname, "/plugin/delete.sh"));
+    } catch (err) {
+      console.log("DB does not exist");
+    }
+  }
+};
