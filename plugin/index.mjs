0c5a723674eeefb924d7a67862bad58dec5e8803
jgoux <hi@jgoux.dev>
2022-09-09 10:02:53 +0200

    test

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -1,5 +1,7 @@
 export const onPreBuild = function ({ netlifyConfig }) {
   if (process.env.CONTEXT === "deploy-preview") {
+    console.log("Hey preview!")
     process.env.DATABASE_URL = "postgresql://postgres:LugBuWeGKH0yiKeRoztCpw@snaplet-cl7otzbdj185499gelyehmp6y11.fly.dev:5432/preview-deploy";
+    netlifyConfig.build.environment.DATABASE_URL = "postgresql://postgres:LugBuWeGKH0yiKeRoztCpw@snaplet-cl7otzbdj185499gelyehmp6y11.fly.dev:5432/preview-deploy";
   }
 };
