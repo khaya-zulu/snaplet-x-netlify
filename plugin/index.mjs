f12384b146ac96dd8ae4995372002a58cfd0b6d4
jgoux <hi@jgoux.dev>
2022-09-09 10:26:08 +0200

    last chance

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -1,9 +1,15 @@
 export const onPreBuild = function ({ netlifyConfig }) {
   switch (process.env.CONTEXT) {
-    case 'production':
+    case "production":
+      console.log("production");
       netlifyConfig.build.environment.DATABASE_URL = process.env.PRODUCTION_DATABASE_URL;
+      process.env.DATABASE_URL = process.env.PRODUCTION_DATABASE_URL;
+      break;
     case "deploy-preview":
+      console.log("preview");
       netlifyConfig.build.environment.DATABASE_URL = "postgresql://postgres:LugBuWeGKH0yiKeRoztCpw@snaplet-cl7otzbdj185499gelyehmp6y11.fly.dev:5432/preview-deploy";
+      process.env.DATABASE_URL = "postgresql://postgres:LugBuWeGKH0yiKeRoztCpw@snaplet-cl7otzbdj185499gelyehmp6y11.fly.dev:5432/preview-deploy";
+      break;
     default:
   }
 };
