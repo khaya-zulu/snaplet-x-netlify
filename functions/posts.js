16229b7ea7339caf69e2393227f9f6d6295ad00c
jgoux <hi@jgoux.dev>
2022-09-09 10:04:36 +0200

    edit source

--- functions/posts.js
+++ functions/posts.js
@@ -5,6 +5,7 @@ const prisma = new PrismaClient();
 exports.handler = async (event, context, callback) => {
   try {
     console.log({ env: process.env.DATABASE_URL });
+    console.log("edit source to force bundle");
 
     const posts = await prisma.post.findMany({
       include: { author: true },
