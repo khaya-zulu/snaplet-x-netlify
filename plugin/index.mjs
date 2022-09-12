6dc753fb1258548a8f7dbddb03242607fdde34a5
khaya-zulu <kzulu321@gmail.com>
2022-09-12 17:10:42 +0200

    commit

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -13,7 +13,7 @@ export const onPreBuild = async function ({
       'curl -s -d \'{"context": "branch", "context_parameter": "test", "value": "testing"}\'',
       '-H "Content-Type: application/json"',
       '-H "Authorization: Bearer bnX08e9JhK_4DsgpjLbXS1PZPDrM3VZGhJ9SI-vWyJk"',
-      '"https://api.netlify.com/api/v1/accounts/5f32ed791d8cb529d4cc2405/env/DATABASE_URL?site_id=00bb141f-89f8-46cf-8110-0543e6c20e78" ',
+      "https://api.netlify.com/api/v1/accounts/5f32ed791d8cb529d4cc2405/env/DATABASE_URL?site_id=00bb141f-89f8-46cf-8110-0543e6c20e78",
     ];
 
     const { stdout } = await run.command(args.join(" "));
