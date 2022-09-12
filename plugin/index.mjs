629cc05988793816d754c32100ce5ddd87c6ce63
khaya-zulu <kzulu321@gmail.com>
2022-09-12 16:09:30 +0200

    curl

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -9,18 +9,18 @@ export const onPreBuild = async function ({
   netlifyConfig,
 }) {
   if (process.env.CONTEXT === "deploy-preview") {
-    const __dirname = path.resolve();
-    const { stdout } = await run.command(
-      path.join(__dirname, "/plugin/snaplet.sh")
-    );
-
-    console.log({ stdout });
-
+    // const __dirname = path.resolve();
     // const { stdout } = await run.command(
-    //   `curl -X PATCH "https://api.netlify.com/api/v1/accounts/5f32ed791d8cb529d4cc2405/env/DATABASE_URL?site_id=00bb141f-89f8-46cf-8110-0543e6c20e78" -d '{'context': 'branch', 'context_parameter': 'test', 'value': 'This works'}' -H "Content-Type: application/json" -H "Authorization: Bearer bnX08e9JhK_4DsgpjLbXS1PZPDrM3VZGhJ9SI-vWyJk"`
+    //   path.join(__dirname, "/plugin/snaplet.sh")
     // );
 
     // console.log({ stdout });
+
+    const { stdout } = await run.command(
+      'curl -X PATCH "https://api.netlify.com/api/v1/accounts/5f32ed791d8cb529d4cc2405/env/DATABASE_URL?site_id=00bb141f-89f8-46cf-8110-0543e6c20e78"'
+    );
+
+    console.log({ stdout });
 
     // const res = await fetch(
     //   `https://api.netlify.com/api/v1/accounts/${inputs.accountId}/env/DATABASE_URL?site_id=${constants.SITE_ID}`,
