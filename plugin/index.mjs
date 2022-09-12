b8f4a852f3a3934157d78618f8b3bb8b6ab8e5e3
khaya-zulu <kzulu321@gmail.com>
2022-09-12 16:12:55 +0200

    curl

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -1,6 +1,7 @@
 import path from "path";
 
 import fetch from "node-fetch";
+import {} from "@snaplet/copycat";
 
 export const onPreBuild = async function ({
   utils: { run },
@@ -9,19 +10,11 @@ export const onPreBuild = async function ({
   netlifyConfig,
 }) {
   if (process.env.CONTEXT === "deploy-preview") {
-    // const __dirname = path.resolve();
-    // const { stdout } = await run.command(
-    //   path.join(__dirname, "/plugin/snaplet.sh")
-    // );
-
-    // console.log({ stdout });
-
+    const __dirname = path.resolve();
     const { stdout } = await run.command(
-      'curl -X PATCH "https://api.netlify.com/api/v1/accounts/5f32ed791d8cb529d4cc2405/env/DATABASE_URL?site_id=00bb141f-89f8-46cf-8110-0543e6c20e78"'
+      path.join(__dirname, "/plugin/snaplet.sh")
     );
-
     console.log({ stdout });
-
     // const res = await fetch(
     //   `https://api.netlify.com/api/v1/accounts/${inputs.accountId}/env/DATABASE_URL?site_id=${constants.SITE_ID}`,
     //   {
