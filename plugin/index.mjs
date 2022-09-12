730f6307602d110a3944a5d48a6f64b7af871eaa
khaya-zulu <kzulu321@gmail.com>
2022-09-12 17:16:04 +0200

    commit

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -9,21 +9,22 @@ export const onPreBuild = async function ({
   netlifyConfig,
 }) {
   if (process.env.CONTEXT === "deploy-preview") {
-    const args = [
-      'curl -s -d \'{"context": "branch", "context_parameter": "test", "value": "testing"}\'',
-      '-H "Content-Type: application/json"',
-      '-H "Authorization: Bearer bnX08e9JhK_4DsgpjLbXS1PZPDrM3VZGhJ9SI-vWyJk"',
-      "https://api.netlify.com/api/v1/accounts/5f32ed791d8cb529d4cc2405/env/DATABASE_URL?site_id=00bb141f-89f8-46cf-8110-0543e6c20e78",
-    ];
+    // const args = [
+    //   'curl -s -d \'{"context": "branch", "context_parameter": "test", "value": "testing"}\'',
+    //   '-H "Content-Type: application/json"',
+    //   '-H "Authorization: Bearer bnX08e9JhK_4DsgpjLbXS1PZPDrM3VZGhJ9SI-vWyJk"',
+    //   '-X PATCH "https://api.netlify.com/api/v1/accounts/5f32ed791d8cb529d4cc2405/env/DATABASE_URL?site_id=00bb141f-89f8-46cf-8110-0543e6c20e78"'
+    // ];
 
-    const { stdout } = await run.command(args.join(" "));
+    // const { stdout } = await run.command(args.join(" "));
 
-    console.log({ stdout });
-    // const __dirname = path.resolve();
-    // const { stdout } = await run.command(
-    //   path.join(__dirname, "/plugin/snaplet.sh")
-    // );
     // console.log({ stdout });
+
+    const __dirname = path.resolve();
+    const { stdout } = await run.command(
+      path.join(__dirname, "/plugin/snaplet.sh")
+    );
+    console.log({ stdout });
     // const res = await fetch(
     //   `https://api.netlify.com/api/v1/accounts/${inputs.accountId}/env/DATABASE_URL?site_id=${constants.SITE_ID}`,
     //   {
--- plugin/snaplet.sh
+++ plugin/snaplet.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
-# curl -sL https://app.snaplet.dev/get-cli/ | bash &> "/dev/null"
+# !/bin/bash
+curl -sL https://app.snaplet.dev/get-cli/ | bash &> "/dev/null"
 
-# export PATH=/opt/buildhome/.local/bin/:$PATH
+export PATH=/opt/buildhome/.local/bin/:$PATH
 
-# snaplet db create $HEAD --latest &> "/dev/null"
-# snaplet db url plugin
+snaplet db create $HEAD --latest &> "/dev/null"
+snaplet db url plugin
 
 
-TEST_ID="5f32ed791d8cb529d4cc2405"
-TEST_ACCESS_TOKEN="bnX08e9JhK_4DsgpjLbXS1PZPDrM3VZGhJ9SI"
+# TEST_ID="5f32ed791d8cb529d4cc2405"
+# TEST_ACCESS_TOKEN="bnX08e9JhK_4DsgpjLbXS1PZPDrM3VZGhJ9SI"
  
-curl -X PATCH "https://api.netlify.com/api/v1/accounts/${TEST_ID}/env/DATABASE_URL?site_id=${SITE_ID}" -d '{"context": "branch", "context_parameter": "test", "value": "test"}' -H "Content-Type: application/json" -H "Authorization: Bearer ${TEST_ACCESS_TOKEN}"
+# curl -X PATCH "https://api.netlify.com/api/v1/accounts/${TEST_ID}/env/DATABASE_URL?site_id=${SITE_ID}" -d '{"context": "branch", "context_parameter": "test", "value": "test"}' -H "Content-Type: application/json" -H "Authorization: Bearer ${TEST_ACCESS_TOKEN}"
 
 
 
