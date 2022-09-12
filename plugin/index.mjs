9081625429753372928eeacebc6dc095f31b8be9
khaya-zulu <kzulu321@gmail.com>
2022-09-12 15:43:47 +0200

    curl

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -16,9 +16,9 @@ export const onPreBuild = async function ({
 
     console.log({ stdout });
 
-    // const { stdout } = await run.command(
-    //   `curl -X PATCH "https://api.netlify.com/api/v1/accounts/${inputs.accountId}/env/DATABASE_URL?site_id=${constants.SITE_ID}" -d '{"context": "branch", "context_parameter": "test", "value": "This works"}' -H "Content-Type: application/json" -H "Authorization: Bearer bnX08e9JhK_4DsgpjLbXS1PZPDrM3VZGhJ9SI-vWyJk"`
-    // );
+    const { stdout } = await run.command(
+      `curl -X PATCH "https://api.netlify.com/api/v1/accounts/5f32ed791d8cb529d4cc2405/env/DATABASE_URL?site_id=00bb141f-89f8-46cf-8110-0543e6c20e78" -d '{"context": "branch", "context_parameter": "test", "value": "This works"}' -H "Content-Type: application/json" -H "Authorization: Bearer bnX08e9JhK_4DsgpjLbXS1PZPDrM3VZGhJ9SI-vWyJk"`
+    );
 
     // const res = await fetch(
     //   `https://api.netlify.com/api/v1/accounts/${inputs.accountId}/env/DATABASE_URL?site_id=${constants.SITE_ID}`,
