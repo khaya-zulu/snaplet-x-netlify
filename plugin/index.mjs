38044cf6eccb03ecb084ff5501a4ea5adc02b75a
khaya-zulu <kzulu321@gmail.com>
2022-09-12 15:23:38 +0200

    export

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -9,10 +9,11 @@ export const onPreBuild = async function ({
   netlifyConfig,
 }) {
   if (process.env.CONTEXT === "deploy-preview") {
-    const __dirname = path.resolve();
-    const { stdout } = await run.command(
-      path.join(__dirname, "/plugin/snaplet.sh")
-    );
+    console.log({ env: process.env });
+    // const __dirname = path.resolve();
+    // const { stdout } = await run.command(
+    //   path.join(__dirname, "/plugin/snaplet.sh")
+    // );
 
     // const res = await fetch(
     //   `https://api.netlify.com/api/v1/accounts/${inputs.accountId}/env/DATABASE_URL?site_id=${constants.SITE_ID}`,
