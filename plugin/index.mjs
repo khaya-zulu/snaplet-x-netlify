791b1bc7c0734e840587ea0aa529e94f16f6a046
khaya-zulu <kzulu321@gmail.com>
2022-09-13 11:22:46 +0200

    commit

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -20,26 +20,26 @@ export const onPreBuild = async function ({
     // const { stdout } = await run("curl", args, { env: process.env });
     // console.log({ stdout });
 
-    const options = {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer bnX08e9JhK_4DsgpjLbXS1PZPDrM3VZGhJ9SI",
-      },
-      body: JSON.stringify({
-        context: "branch",
-        context_parameter: "khaya_working_01",
-        value: "supersecretttt",
-      }),
-    };
+    // const options = {
+    //   method: "PATCH",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     Authorization: "Bearer HsPVz8_QzUmG41jSrwTxqPbRDelFcUl1VXcNg9wBHlE",
+    //   },
+    //   body: JSON.stringify({
+    //     context: "branch",
+    //     context_parameter: "khaya_working_01",
+    //     value: "supersecretttt",
+    //   }),
+    // };
 
-    fetch(
-      "https://api.netlify.com/api/v1/accounts/5f32ed791d8cb529d4cc2405/env/DATABASE_URL?site_id=00bb141f-89f8-46cf-8110-0543e6c20e78",
-      options
-    )
-      .then((response) => response.json())
-      .then((response) => console.log(response))
-      .catch((err) => console.error(err));
+    // fetch(
+    //   "https://api.netlify.com/api/v1/accounts/5f32ed791d8cb529d4cc2405/env/DATABASE_URL?site_id=00bb141f-89f8-46cf-8110-0543e6c20e78",
+    //   options
+    // )
+    //   .then((response) => response.json())
+    //   .then((response) => console.log(response))
+    //   .catch((err) => console.error(err));
 
     // const __dirname = path.resolve();
     // const { stdout } = await run.command(
@@ -48,24 +48,24 @@ export const onPreBuild = async function ({
     // );
     // console.log({ stdout });
 
-    // fetch(
-    //   `https://api.netlify.com/api/v1/accounts/5f32ed791d8cb529d4cc2405/env/DATABASE_URL?site_id=00bb141f-89f8-46cf-8110-0543e6c20e78`,
-    //   {
-    //     method: "PATCH",
-    //     body: JSON.stringify({
-    //       context: "branch",
-    //       context_parameter: "working",
-    //       value: "testing",
-    //     }),
-    //     headers: {
-    //       Authorization: `Bearer bnX08e9JhK_4DsgpjLbXS1PZPDrM3VZGhJ9SI`,
-    //       // Authorization: `Bearer ${process.env.API_ACCESS_TOKEN}`,
-    //       "Content-Type": "application/json",
-    //     },
-    //   }
-    // )
-    //   .then((res) => res.json())
-    //   .catch((err) => console.error(err));
+    fetch(
+      `https://api.netlify.com/api/v1/accounts/5f32ed791d8cb529d4cc2405/env/DATABASE_URL?site_id=00bb141f-89f8-46cf-8110-0543e6c20e78`,
+      {
+        method: "PATCH",
+        body: JSON.stringify({
+          context: "branch",
+          context_parameter: "juliens_token",
+          value: "testing",
+        }),
+        headers: {
+          Authorization: `Bearer HsPVz8_QzUmG41jSrwTxqPbRDelFcUl1VXcNg9wBHlE`,
+          // Authorization: `Bearer ${process.env.API_ACCESS_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => res.json())
+      .catch((err) => console.error(err));
     // console.log({ res: JSON.stringify(res, undefined, 2) });
   }
 };
