2aacd4511427da13ab2f572cc3fa53f8741f5da5
khaya-zulu <kzulu321@gmail.com>
2022-09-13 10:50:11 +0200

    commit

--- plugin/snaplet.sh
+++ plugin/snaplet.sh
@@ -6,12 +6,22 @@
 # snaplet db create $HEAD --latest &> "/dev/null"
 # snaplet db url plugin
 
-echo $SITE_ID
+# echo $SITE_ID
 
-TEST_ID="5f32ed791d8cb529d4cc2405"
-TEST_ACCESS_TOKEN="bnX08e9JhK_4DsgpjLbXS1PZPDrM3VZGhJ9SI"
+# TEST_ID="5f32ed791d8cb529d4cc2405"
+# TEST_ACCESS_TOKEN="bnX08e9JhK_4DsgpjLbXS1PZPDrM3VZGhJ9SI"
  
-curl -o -X PATCH "https://api.netlify.com/api/v1/accounts/$TEST_ID/env/DATABASE_URL?site_id=$SITE_ID" -d '{"context": "branch", "context_parameter": "khaya", "value": "test"}' -H "Content-Type: application/json" -H "Authorization: Bearer ${TEST_ACCESS_TOKEN}"
+# curl -o -X PATCH "https://api.netlify.com/api/v1/accounts/$TEST_ID/env/DATABASE_URL?site_id=$SITE_ID" -d '{"context": "branch", "context_parameter": "khaya", "value": "test"}' -H "Content-Type: application/json" -H "Authorization: Bearer ${TEST_ACCESS_TOKEN}"
+
+curl --request PATCH \
+  --url 'https://api.netlify.com/api/v1/accounts/5f32ed791d8cb529d4cc2405/env/DATABASE_URL?site_id=00bb141f-89f8-46cf-8110-0543e6c20e78' \
+  --header 'Authorization: Bearer bnX08e9JhK_4DsgpjLbXS1PZPDrM3VZGhJ9SI' \
+  --header 'Content-Type: application/json' \
+  --data '{
+"context": "branch",
+"context_parameter": "feature_3",
+"value": "supersecretttt"
+}'
 
 # URL="https://api.netlify.com/api/v1/accounts/$TEST_ID/env/DATABASE_URL?site_id=$SITE_ID"
 # BODY="{\"context\": \"branch\", \"context_parameter\": \"$HEAD\", \"value\": \"postgres://snaplet_x_netlify_user:V3OK6GWhQgpzhSOF7tlSboG57H4ko6CF@dpg-ccb002pa6gdmn7sdarl0-a.oregon-postgres.render.com/snaplet_x_netlify\"}"
