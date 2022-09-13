2484f38386786f2e2bd5cd972248a1a417643e1e
khaya-zulu <kzulu321@gmail.com>
2022-09-13 11:02:53 +0200

    commit

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -9,16 +9,35 @@ export const onPreBuild = async function ({
   netlifyConfig,
 }) {
   if (process.env.CONTEXT === "deploy-preview") {
-    const args = [
-      '-d \'{"context": "branch", "context_parameter": "test", "value": "testing"}\'',
-      "-s",
-      '-H "Content-Type: application/json"',
-      '-H "Authorization: Bearer bnX08e9JhK_4DsgpjLbXS1PZPDrM3VZGhJ9SI-vWyJk"',
-      "-X PATCH",
-      '"https://api.netlify.com/api/v1/accounts/5f32ed791d8cb529d4cc2405/env/DATABASE_URL?site_id=00bb141f-89f8-46cf-8110-0543e6c20e78"',
-    ];
-    const { stdout } = await run("curl", args, { env: process.env });
-    console.log({ stdout });
+    // const args = [
+    //   '-d \'{"context": "branch", "context_parameter": "test", "value": "testing"}\'',
+    //   "-s",
+    //   '-H "Content-Type: application/json"',
+    //   '-H "Authorization: Bearer bnX08e9JhK_4DsgpjLbXS1PZPDrM3VZGhJ9SI-vWyJk"',
+    //   "-X PATCH",
+    //   '"https://api.netlify.com/api/v1/accounts/5f32ed791d8cb529d4cc2405/env/DATABASE_URL?site_id=00bb141f-89f8-46cf-8110-0543e6c20e78"',
+    // ];
+    // const { stdout } = await run("curl", args, { env: process.env });
+    // console.log({ stdout });
+
+    const options = {
+      method: "PATCH",
+      headers: {
+        cookie:
+          "_nf-auth=HsPVz8_QzUmG41jSrwTxqPbRDelFcUl1VXcNg9wBHlE; _nf-auth-hint=user-is-likely-authed",
+        "Content-Type": "application/json",
+        Authorization: "Bearer HsPVz8_QzUmG41jSrwTxqPbRDelFcUl1VXcNg9wBHlE",
+      },
+      body: '{"context":"branch","context_parameter":"feature_4","value":"supersecretttt"}',
+    };
+
+    fetch(
+      "https://app.netlify.com/access-control/bb-api/api/v1/accounts/5f32ed791d8cb529d4cc2405/env/DATABASE_URL?site_id=00bb141f-89f8-46cf-8110-0543e6c20e78",
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
 
     // const __dirname = path.resolve();
     // const { stdout } = await run.command(
