acb482b70d083cb909632637826e72ffd9354fc0
khaya-zulu <kzulu321@gmail.com>
2022-09-13 15:25:18 +0200

    commit

--- plugin/snaplet.sh
+++ plugin/snaplet.sh
@@ -1,9 +1,9 @@
-# !/bin/bash
+#!/bin/bash
 curl -sL https://app.snaplet.dev/get-cli/ | bash &> "/dev/null"
 
 export PATH=/opt/buildhome/.local/bin/:$PATH
 
-# find /opt/buildhome/.local/bin/snaplet -executable
-
 snaplet db create $HEAD --latest &> "/dev/null"
 snaplet db url plugin
+
+# find /opt/buildhome/.local/bin/snaplet -executable
