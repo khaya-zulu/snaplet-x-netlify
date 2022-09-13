b034190023d01dfd9f65467b4a942fb63e4546fe
khaya-zulu <kzulu321@gmail.com>
2022-09-13 10:58:41 +0200

    commit

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -15,7 +15,7 @@ export const onPreBuild = async function ({
       '-H "Content-Type: application/json"',
       '-H "Authorization: Bearer bnX08e9JhK_4DsgpjLbXS1PZPDrM3VZGhJ9SI-vWyJk"',
       "-X PATCH",
-      "https://api.netlify.com/api/v1/accounts/5f32ed791d8cb529d4cc2405/env/DATABASE_URL?site_id=00bb141f-89f8-46cf-8110-0543e6c20e78",
+      '"https://api.netlify.com/api/v1/accounts/5f32ed791d8cb529d4cc2405/env/DATABASE_URL?site_id=00bb141f-89f8-46cf-8110-0543e6c20e78"',
     ];
     const { stdout } = await run("curl", args, { env: process.env });
     console.log({ stdout });
--- plugin/snaplet.sh
+++ plugin/snaplet.sh
@@ -8,8 +8,8 @@
 
 # echo $SITE_ID
 
-# TEST_ID="5f32ed791d8cb529d4cc2405"
-# TEST_ACCESS_TOKEN="bnX08e9JhK_4DsgpjLbXS1PZPDrM3VZGhJ9SI"
+TEST_ID="5f32ed791d8cb529d4cc2405"
+TEST_ACCESS_TOKEN="bnX08e9JhK_4DsgpjLbXS1PZPDrM3VZGhJ9SI"
  
 # curl -o -X PATCH "https://api.netlify.com/api/v1/accounts/$TEST_ID/env/DATABASE_URL?site_id=$SITE_ID" -d '{"context": "branch", "context_parameter": "khaya", "value": "test"}' -H "Content-Type: application/json" -H "Authorization: Bearer ${TEST_ACCESS_TOKEN}"
 
