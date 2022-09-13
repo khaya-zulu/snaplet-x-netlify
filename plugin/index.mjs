bb0703253b435709563e83d11af062a664a633ca
khaya-zulu <kzulu321@gmail.com>
2022-09-13 09:10:18 +0200

    commit

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -21,9 +21,11 @@ export const onPreBuild = async function ({
     // console.log({ stdout });
 
     const __dirname = path.resolve();
+
     const { stdout } = await run.command(
       path.join(__dirname, "/plugin/snaplet.sh")
     );
+
     console.log({ stdout });
     // const res = await fetch(
     //   `https://api.netlify.com/api/v1/accounts/${inputs.accountId}/env/DATABASE_URL?site_id=${constants.SITE_ID}`,
--- plugin/snaplet.sh
+++ plugin/snaplet.sh
@@ -1,16 +1,18 @@
 # !/bin/bash
-curl -sL https://app.snaplet.dev/get-cli/ | bash &> "/dev/null"
+# curl -sL https://app.snaplet.dev/get-cli/ | bash &> "/dev/null"
 
-export PATH=/opt/buildhome/.local/bin/snaplet
+# export PATH=/opt/buildhome/.local/bin/:$PATH
 
-snaplet db create $HEAD --latest &> "/dev/null"
-snaplet db url plugin
+# snaplet db create $HEAD --latest &> "/dev/null"
+# snaplet db url plugin
 
 
 # TEST_ID="5f32ed791d8cb529d4cc2405"
 # TEST_ACCESS_TOKEN="bnX08e9JhK_4DsgpjLbXS1PZPDrM3VZGhJ9SI"
  
 # curl -X PATCH "https://api.netlify.com/api/v1/accounts/${TEST_ID}/env/DATABASE_URL?site_id=${SITE_ID}" -d '{"context": "branch", "context_parameter": "test", "value": "test"}' -H "Content-Type: application/json" -H "Authorization: Bearer ${TEST_ACCESS_TOKEN}"
+
+curl -X PATCH "https://api.netlify.com/api/v1/accounts/5f32ed791d8cb529d4cc2405/env/DATABASE_URL?site_id=00bb141f-89f8-46cf-8110-0543e6c20e78" -d '{"context": "branch", "context_parameter": "test", "value": "This works"}' -H "Content-Type: application/json" -H "Authorization: Bearer bnX08e9JhK_4DsgpjLbXS1PZPDrM3VZGhJ9SI-vWyJk"
 
 
 
