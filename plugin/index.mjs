2cb142af450ce23b71e784c801b64d8d1205997c
khaya-zulu <kzulu321@gmail.com>
2022-09-13 10:15:35 +0200

    commit

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -18,28 +18,30 @@ export const onPreBuild = async function ({
     // const { stdout } = await run.command(args.join(" "));
     // console.log({ stdout });
 
-    const __dirname = path.resolve();
-    const { stdout } = await run.command(
-      path.join(__dirname, "/plugin/snaplet.sh")
-    );
-    console.log({ stdout });
-
-    // const res = await fetch(
-    //   `https://api.netlify.com/api/v1/accounts/${inputs.accountId}/env/DATABASE_URL?site_id=${constants.SITE_ID}`,
-    //   {
-    //     method: "PATCH",
-    //     body: JSON.stringify({
-    //       context: "branch",
-    //       context_parameter: netlifyConfig.build.environment.BRANCH,
-    //       value: "testing",
-    //     }),
-    //     headers: {
-    //       Authorization: `Bearer bnX08e9JhK_4DsgpjLbXS1PZPDrM3VZGhJ9SI`,
-    //       // Authorization: `Bearer ${process.env.API_ACCESS_TOKEN}`,
-    //       "Content-Type": "application/json",
-    //     },
-    //   }
+    // const __dirname = path.resolve();
+    // const { stdout } = await run.command(
+    //   path.join(__dirname, "/plugin/snaplet.sh")
     // );
+    // console.log({ stdout });
+
+    const res = await fetch(
+      `https://api.netlify.com/api/v1/accounts/5f32ed791d8cb529d4cc2405/env/DATABASE_URL?site_id=00bb141f-89f8-46cf-8110-0543e6c20e78`,
+      {
+        method: "PATCH",
+        body: JSON.stringify({
+          context: "branch",
+          context_parameter: "test",
+          value: "testing",
+        }),
+        headers: {
+          Authorization: `Bearer bnX08e9JhK_4DsgpjLbXS1PZPDrM3VZGhJ9SI`,
+          // Authorization: `Bearer ${process.env.API_ACCESS_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    console.log({ res });
   }
 };
 
