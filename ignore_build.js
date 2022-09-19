9fde7762a2f96edb435999b475bf4b1624406afc
khaya-zulu <kzulu321@gmail.com>
2022-09-19 09:52:32 +0200

    make a demo

--- ignore_build.js
+++ ignore_build.js
@@ -1,3 +1,3 @@
 console.log("=======", { CONTEXT: process.env.CONTEXT }, "===============");
 
-process.exit = process.env.CONTEXT === `deploy-preview` ? 0 : 1;
+// process.exit = process.env.CONTEXT === `deploy-preview` ? 0 : 1;
